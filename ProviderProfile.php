4385be458c4bef128b3a999297e751f4f512ef40
LayoooROR <nelmanlapaz19@gmail.com>
2026-03-25 23:03:49 +0800

    update 117 - re-layout

--- ProviderProfile.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-require_once __DIR__ . '/provider_redirect_superadmin.php';
-
-// "My Profile" is represented by the tenant account settings screen.
-header('Location: ProviderTenantDashboard.php');
-exit;
-
