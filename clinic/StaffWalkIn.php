efc1e21df0155b344624f1de29baccfc58cf5b50
LayoooROR <nelmanlapaz19@gmail.com>
2026-04-26 14:50:07 +0800

    ROBLOX BLOCK TIME

--- clinic/StaffWalkIn.php
+++ clinic/StaffWalkIn.php
@@ -215,7 +215,7 @@ try {
 <head>
     <meta charset="utf-8"/>
     <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
-    <title>Walk-In Booking | Clinical Precision</title>
+    <title>Walk-In Bookings | Clinical Precision</title>
     <link href="https://fonts.googleapis.com" rel="preconnect"/>
     <link crossorigin="" href="https://fonts.gstatic.com" rel="preconnect"/>
     <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700;800&amp;display=swap" rel="stylesheet"/>
