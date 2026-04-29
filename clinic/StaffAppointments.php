3138e5ef4af12770acacfba12296aafb2b809c1b
LayoooROR <nelmanlapaz19@gmail.com>
2026-04-30 00:57:41 +0800

    POTAH AYAW PA

--- clinic/StaffAppointments.php
+++ clinic/StaffAppointments.php
@@ -851,7 +851,7 @@ if ($currentTenantSlug !== '') {
                         Daily schedule with status tracking and treatment details.
                     </p>
                 </div>
-                <div class="shrink-0 pt-1">
+                <div class="shrink-0 pt-5">
                     <button
                         type="button"
                         id="openBookingTypeModalBtn"
